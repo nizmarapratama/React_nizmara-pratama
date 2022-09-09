18edc3b2183d8aad3616df544de29a5050a40e0f
nizmarapratama <nizmarapratama.@gmail.com>
2022-09-09 23:27:12 +0700

    findMypet

--- 08-2_Card/algoritma/algoritma.js
+++ 08-2_Card/algoritma/algoritma.js
@@ -1,5 +1,14 @@
-const findMyPet = (nama) => {
+const findMypet = (nama) => {
+    let hewan = [];
 
-}
+    const pets = myPet.map((pet) => {
+        if (nama === pet) {
+            hewan = nama;
+        }
+    });
+    console.log(hewan + "ada di ruangan 1");
+};
 
-const myPets = ["Ayam","Ikan","Iguana","Kecoak"]
+const myPet = ["ayam", "ikan", "iguana", "kecoak"];
+
+findMypet("ayam");
