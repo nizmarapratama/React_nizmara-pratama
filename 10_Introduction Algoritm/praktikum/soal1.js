57ea9bfd8d3d85587cf467007626ca70529e602c
nizmarapratama <nizmarapratama.@gmail.com>
2022-09-11 00:36:09 +0700

    Problem

--- 10_Introduction Algoritm/praktikum/soal1.js
+++ 10_Introduction Algoritm/praktikum/soal1.js
@@ -1,17 +1,21 @@
 function bilanganPrima(angka) {
     if (angka <= 1) {
-        return console.log("angka harus lebih besar dari 1");
+      return console.log("angka harus lebih besar dari 1");
     }
     if (angka % 2 === 0 || angka % 3 === 0) {
-        if (angka / 2 === 1 || angkat / 3 === 1) {
-            return console.log('${angka} adalah bilangan prima');
-        } else {
-            return console.log('${angka} bukan bilangan prima');
-        }
-    } else {
+      if (angka / 2 === 1 || angka / 3 === 1) {
         return console.log('${angka} adalah bilangan prima');
+      } else {
+        return console.log('${angka} bukan bilangan prima');
+      }
+    } else {
+      return console.log('${angka} adalah bilangan prima');
     }
-}
-
-const angka = 10;
-bilanganPrima(angka);
+  }
+  
+  /**
+   * Sample Test Cases
+  // Input 3, Input 7, Input 10
+   */
+  const angka = 10;
+  bilanganPrima(angka);
---
+++ 10_Introduction Algoritm/praktikum/soal2.js
@@ -0,0 +1,16 @@
+function lampuTombol(n) {
+    let lampu = false;
+    for (let i = 0; i < n + 1; i++) {
+        if (n % i === 0) {
+            lampu = !lampu;
+        }
+    }
+    if (lampu) {
+        return console.log("lampu menyala");
+    } else {
+        return console.log("lampu mati");
+    }
+}
+
+// 
+lampuTombol(4);
