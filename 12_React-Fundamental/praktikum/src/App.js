9875c64c0e302a6e224d748040af45a890cf2564
nizmarapratama <nizmarapratama.@gmail.com>
2022-09-17 16:59:24 +0700

    Todo App

--- 12_React-Fundamental/praktikum/src/App.js
+++ 12_React-Fundamental/praktikum/src/App.js
@@ -1,13 +1,41 @@
-import React from "react"; 
-import './App.css';
-import Todo from "./pages/Todo"
+import React, { useState } from "react";
 
-class App extends React.Component {
-  render () {
-    return <div className="App">
-      <Todo />
+import Form from "./Form";
+import "./App.css";
+
+export default () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (i) =>
+    setTodos(
+      todos.map((todo, k) =>
+        k === i
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    );
+
+  return (
+    <div className="App">
+      <h1>TODO APP</h1>
+      <Form onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])} />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : "",
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>reset</button>
     </div>
-  }
-}
-
-export default App;
+  );
+};
