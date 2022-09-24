fd94bc9e2ed4b7775e06f112bcdc0769f1018a97
nizmarapratama <nizmarapratama.@gmail.com>
2022-09-25 01:31:42 +0700

    Todo Hooks

---
+++ 14_React-hook-introduction/praktikum/src/components/ToDoList.js
@@ -0,0 +1,23 @@
+const ToDoList = ({data, hapusToDo, handleChange}) => {
+    return ( 
+      <div style={{margin: "10px"}}>
+          {data.map((todo) => (
+            <div className="todo-list" key={todo.id}>
+              <input 
+              type="checkbox" 
+              name="todo" 
+              id="todo" 
+              checked={todo.completed}
+              onChange={() => {handleChange(todo.id)}}/>
+              <p style={todo.completed?{textDecoration:"line-through", color:"grey"}:{textDecoration:"none"}}>{todo.title}</p>
+              <button onClick={() => {
+                hapusToDo(todo.id)
+              }}
+              className="btn-delete-list">Delete</button>
+            </div>
+          ))}
+      </div>
+     );
+  }
+   
+  export default ToDoList;
