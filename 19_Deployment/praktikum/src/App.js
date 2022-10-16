1c76ae93192b73a079329222083a5f1bc5d068c7
nizmarapratama <nizmarapratama.@gmail.com>
2022-10-16 23:34:12 +0700

    Deployment

--- 19_Deployment/praktikum/src/App.js
+++ 19_Deployment/praktikum/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Selamat Datang di Website Nizmara Pratama
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="deployment-ara.surge.sh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          deployment
         </a>
       </header>
     </div>
